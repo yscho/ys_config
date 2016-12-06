fac8770d12b8669a36473e99ad09c47c78bedea5
Youngsoo_cho <youngsoo_cho@tmax.co.kr>
2016-12-06 18:00:09 +0900

    echo 수정

--- local/install_vundle.sh
+++ local/install_vundle.sh
@@ -4,4 +4,4 @@
 #http://sunyzero.tistory.com/212 ...reference
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo Please add script in your "~/.vimrc" file about Vundle....
+echo "Please add script in your [~/.vimrc] file about Vundle...."
