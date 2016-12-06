0c2eec6ff3473fcc5443ddfc0cdd534c260398fe
Youngsoo_cho <youngsoo_cho@tmax.co.kr>
2016-12-06 17:47:38 +0900

    vundle install file

---
+++ local/install_vundle.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+
+#http://sunyzero.tistory.com/212 ...reference
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo Please add script in your "~/.vimrc" file about Vundle....
